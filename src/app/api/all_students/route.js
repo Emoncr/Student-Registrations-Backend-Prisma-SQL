775f3c596b08ef8737b7342cfda54bbef959393c
Emoncr <biplobemon 75945@gmail.com>
2023-12-24 03:46:44 +0600

    get single student api

--- src/app/api/all_students/route.js
+++ src/app/api/all_students/route.js
@@ -1,6 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
+// ========== Api FOR Find All students ============//
+export const GET = async (req, res) => {
+  const prisma = new PrismaClient();
+  try {
+    const result = await prisma.Users.findMany();
+    return NextResponse.json(
+      {
+        message: "Success",
+        result,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Failed!",
+        error: error.meta.cause,
+      },
+      { status: 404 }
+    );
+  }
+};
+
 export const POST = async (req, res) => {
   const reqBody = await req.json();
   const prisma = new PrismaClient();
